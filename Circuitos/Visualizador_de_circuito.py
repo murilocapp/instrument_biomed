29230b5c18b971a4779fca06e810ddc04970f304
murilocapp <mu.appugliese@gmail.com>
2024-04-04 18:47:55 -0300

    ultimo do dia

--- Circuitos/Visualizador_de_circuito.py
+++ Circuitos/Visualizador_de_circuito.py
@@ -1,8 +1,11 @@
 import argparse
+import random
+from typing import Mapping
 import networkx as nx
 import matplotlib.pyplot as plt
 import PIL
 from os import path
+
 
 def create_icons() -> dict[str, str]:
     icons = {
@@ -12,6 +15,7 @@ def create_icons() -> dict[str, str]:
         'C' : path.join('icons', 'Capacitor.png'),
         'V' : path.join('icons', 'FonteTensao.png'),
         'Arduino': path.join('icons', 'Arduino.png'),
+        'node': path.join('icons', 'node.webp'),
     }
 
     images = {k: PIL.Image.open(fname) for k, fname in icons.items()}
@@ -50,6 +54,7 @@ class Grafo(nx.Graph):
     def criar_grafo_complexo(self) -> nx.Graph:
         extra = []
         icons = create_icons()
+        comp_nodes = []
         with open(self.arquivo, 'r') as f:
             for linha in f:
                 if linha.startswith(".") or linha.startswith("*") or linha.startswith("$"):
@@ -58,16 +63,23 @@ class Grafo(nx.Graph):
                     componente, *nos, valor = linha.split()
                     if componente.startswith("X"):
                         _componente = componente.split('.')[1]
-                        self.G.add_node(_componente, image=icons[_componente.split(sep="_")[0]], label=valor)
+                        self.G.add_node(_componente,image=icons[_componente.split(sep="_")[0]], label=valor)
+                        comp_nodes.append(_componente)
                         for n in nos[:-1]:  
+                            self.G.add_node(n, name= n, image=icons['node'])
                             self.G.add_edge(n, _componente)
+                        self.G.add_node(nos[-1], name = nos[-1] , image=icons['node'])
                         self.G.add_edge(_componente, nos[-1])
                     else:
                         self.G.add_node(componente, image=icons.get(componente[0]), label=valor)
+                        comp_nodes.append(componente)
                         nIn, nOut = nos[0], nos[-1]  
+                        self.G.add_node(nIn, name = nIn, image=icons['node'])
+                        self.G.add_node(nOut, name = nOut, image=icons['node'])
                         self.G.add_edge(nIn, componente)
                         self.G.add_edge(componente, nOut)
         self.extra = extra
+        self.comp_nodes = comp_nodes
         return self.G
 
 class Draw:
@@ -75,10 +87,23 @@ class Draw:
         pass
     
     @staticmethod
-    def desenhar_grafo(G: nx.Graph):
-        pos = nx.spring_layout(G, seed=1734289230)
-        fig, ax = plt.subplots()
+    def desenhar_grafo_complexo(G: nx.Graph):
+        # Define o layout com o nó '0' no centro
+        seed = 63502528
+        k = 1.0 / (1e3) ** 0.5
+        pos = nx.spring_layout(G, seed=seed, k=k)
 
+        # Encontrar as coordenadas médias de todos os nós
+        x_values = [pos[node][0] for node in G.nodes()]
+        y_values = [pos[node][1] for node in G.nodes()]
+        x_center = sum(x_values)*5e15 / (len(G.nodes())) 
+        y_center = sum(y_values)*5e15 / (len(G.nodes()))
+
+        # Ajustar todas as posições dos nós subtraindo as coordenadas do centro
+        pos = {node: (x-x_center, y-y_center) for node, (x, y) in pos.items()}
+        fig, ax = plt.subplots()
+        #configurar titulo com a seed
+        plt.title(f"x_center {x_center}  y_center {y_center}")
         # Note: the min_source/target_margin kwargs only work with FancyArrowPatch objects.
         # Force the use of FancyArrowPatch for edge drawing by setting `arrows=True`,
         # but suppress arrowheads with `arrowstyle="-"`
@@ -88,8 +113,9 @@ class Draw:
             ax=ax,
             arrows=True,
             arrowstyle="-",
-            min_source_margin=15,
-            min_target_margin=15,
+            connectionstyle="arc3",
+            min_source_margin=0.5,
+            min_target_margin=0.5,
         )
 
         # Transform from data coordinates (scaled between xlim and ylim) to display coordinates
@@ -110,6 +136,11 @@ class Draw:
                 a = plt.axes([xa - icon_center, ya - icon_center, icon_size, icon_size])
                 a.imshow(G.nodes[n]["image"])
                 a.axis("off")
+            if "label" in G.nodes[n]:
+                plt.text(pos[n][0], pos[n][1], G.nodes[n]["label"], fontsize=6)
+            if "name" in G.nodes[n]:
+                plt.text(pos[n][0], pos[n][1], G.nodes[n]["name"], fontsize=6)
+
         plt.show()
 
 @staticmethod
@@ -120,7 +151,7 @@ def main():
 
     G = Grafo(args.circ)
     G = G.criar_grafo_complexo()
-    Draw.desenhar_grafo(G)
+    Draw.desenhar_grafo_complexo(G)
 
 
 if __name__ == "__main__":
